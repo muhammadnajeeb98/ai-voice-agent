1d21048e0a1ecf7cc4064ebc1d7484d5cfb9989e
muhammadnajeeb98 <muhnaj98@gmail.com>
2025-01-21 19:34:05 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Dashboard from './components/Dashboard';
+import Pricing from './components/Pricing';
+import LanguageSwitcher from './components/LanguageSwitcher';
+import './i18n';
 
 function App() {
   return (
     <Router>
+      <LanguageSwitcher />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </Router>
   );
